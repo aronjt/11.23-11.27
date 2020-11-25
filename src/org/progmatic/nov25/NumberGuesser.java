1c31ed885e3860d7f296760ae2db33db9cebe2d7
aronjt <jtelekiaron@gmail.com>
2020-11-25 13:12:57 +0100

    NumberGuesser

---
+++ src/org/progmatic/nov25/NumberGuesser.java
@@ -0,0 +1,33 @@
+package org.progmatic.nov25;
+
+import java.util.Scanner;
+
+public class NumberGuesser {
+
+    public static void main(String[] args) {
+        int num = (int)(Math.random()*101);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gondoltam egy számra 0 és 100 között, táláld ki te fasz");
+        int guess;
+        do {
+            try {
+                guess = sc.nextInt();
+            }
+            catch (Exception e) {
+                System.out.println("Számot írj be te gyökér");
+                sc = new Scanner(System.in);
+                guess = sc.nextInt();
+            }
+            if (guess < num) {
+                System.out.println("Nagyobb számra gondoltam");
+                System.out.print("Tipp: ");
+            }
+            if (guess > num){
+                System.out.println("Kisebb számra gondoltam");
+                System.out.print("Tipp: ");
+            }
+
+        } while (guess != num);
+        System.out.println("Kitaláltad");
+    }
+}
