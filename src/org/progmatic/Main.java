b2fb4cfb369a2a3d859200662b5933d42d09d907
aronjt <jtelekiaron@gmail.com>
2020-11-27 14:40:47 +0100

    numberBet, half player game

--- src/org/progmatic/Main.java
+++ src/org/progmatic/Main.java
@@ -2,14 +2,15 @@ package org.progmatic;
 
 import org.progmatic.nov27.Casino;
 import org.progmatic.nov27.Martingel;
+import org.progmatic.nov27.RandomPerson;
 import org.progmatic.nov27.RouletteTable;
 
 public class Main {
     public static void main(String[] args) {
-        RouletteTable rouletteTable = new RouletteTable(100, 1000000);
+        RouletteTable rouletteTable = new RouletteTable(100, 10000);
         Martingel martingel = new Martingel(10000, rouletteTable.getMinBet());
         Casino casino = new Casino();
-        casino.rouletteGame(rouletteTable);
-
+        RandomPerson randomPerson = new RandomPerson(50000);
+        casino.rouletteSimulation(randomPerson, rouletteTable);
     }
 }
